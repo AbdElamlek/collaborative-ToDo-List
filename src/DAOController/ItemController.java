ecb2851bdd4de093a6196af8e456d7e885fdec8a
RehamEzzat <rehamezzatmohamed@gmail.com>
2019-12-31 16:00:56 +0200

    Comment\ItemController implementation

--- src/DAOController/ItemController.java
+++ src/DAOController/ItemController.java
@@ -5,9 +5,13 @@
  */
 package DAOController;
 
+import Connection.DataBaseConnection;
 import DAOs.BaseDAO;
-import Entities.BaseEntity;
 import Entities.ItemEntity;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -15,30 +19,92 @@ import java.util.ArrayList;
  * @author Abd-Elmalek
  */
 public class ItemController<ItemDAO> implements BaseDAO<ItemEntity>{
-
+    Connection connection = DataBaseConnection.getInstance();
+    PreparedStatement preparedStatement;
+    
     @Override
     public ArrayList<ItemEntity> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<ItemEntity> items = new ArrayList<ItemEntity>();
+        try {
+            String query = "SELECT * FROM item";
+            preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next()){
+                items.add(new ItemEntity(resultSet.getInt("id"), resultSet.getString("title"), resultSet.getString("description"), resultSet.getInt("todoId")));
+            }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        return items;
     }
 
     @Override
     public ItemEntity findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String query = "SELECT * FROM [todoDB].[dbo].[item] WHERE id = ?";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            if(resultSet.next())
+                return new ItemEntity(resultSet.getInt("id"), resultSet.getString("title"), resultSet.getString("description"), resultSet.getInt("todoId"));
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } 
+       return null;
     }
 
     @Override
     public boolean insert(ItemEntity entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String query = "INSERT INTO [todoDB].[dbo].[item] (title, description, todoId) VALUES (?, ?, ?)";
+            preparedStatement = connection.prepareStatement(query);
+            
+            preparedStatement.setString(1, entity.getTitle());
+            preparedStatement.setString(2, entity.getDescription());
+            preparedStatement.setInt(3, entity.getTodoId());
+            
+            return (preparedStatement.executeUpdate() > 0);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } 
+        return false;
     }
 
     @Override
     public boolean update(ItemEntity entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String query = "UPDATE [todoDB].[dbo].[item] SET title = ?, description = ?, todoId = ? WHERE id = ?";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, entity.getTitle());
+            preparedStatement.setString(2, entity.getDescription());
+            preparedStatement.setInt(3, entity.getTodoId());
+            preparedStatement.setInt(4, entity.getId());
+            
+            return (preparedStatement.executeUpdate() > 0);          
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        return false;
     }
 
     @Override
     public boolean delete(ItemEntity entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String query = "DELETE FROM [todoDB].[dbo].[item] WHERE id = ?";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, entity.getId());
+            
+            return (preparedStatement.executeUpdate() > 0);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
     }
 
 
